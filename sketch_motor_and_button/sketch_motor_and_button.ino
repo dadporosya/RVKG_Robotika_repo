ade220dd73a3cc582828aa19f707c5b7fb6e90e1
RVKG I-P <RVKG@rklasg-123-33.(none)>
2026-04-10 12:57:38 +0300

    btn test class

--- sketch_motor_and_button/sketch_motor_and_button.ino
+++ sketch_motor_and_button/sketch_motor_and_button.ino
@@ -1,38 +1,143 @@
-int BTN1_PIN = 7;
+class Button {
+  private:
+    int PIN;
+    bool hold = false;
+
+    void (*onLow)();
+    void (*onEnteringLow)();
+
+    void (*onUp)();
+    void (*onEnteringUp)();
+
+  public:
+    void Init(
+        int pinIn,
+        void (*lowIn)(void*),
+        void (*enterLowIn)(void*),
+        void (*upIn)(void*),
+        void (*enterUpIn)(void*)
+    ){
+      PIN = pinIn;
+
+      onLow = lowIn;
+      onEnteringLow = enterLowIn;
+
+      onUp = upIn;
+      onEnteringUp = enterUpIn;
+
+    }
+
+    // void Init(
+    //     int pinIn,
+    //     setup()::<lambda()>,
+    //     setup()::<lambda()>,
+    //     setup()::<lambda()>,
+    //     setup()::<lambda()>
+    // ){
+    //   PIN = pinIn;
+
+    //   onLow = lowIn;
+    //   onEnteringLow = enterLowIn;
+
+    //   onUp = upIn;
+    //   onEnteringUp = enterUpIn;
+    // }
+
+    void Update(){
+      if (!IsPressed()) {
+        if (!hold) {
+          if (onUp) onUp();
+          return;
+        }
+        hold = false;
+        if (onEnteringUp) onEnteringUp();
+        return;
+      }
+
+      if (hold){
+        if (onLow) onLow();
+        return;
+      }
+
+      hold = true;
+      if (onEnteringLow) onEnteringLow();
+      return;
+
+    }
+
+    bool IsPressed(){
+      return digitalRead(PIN) == LOW;
+    }
+};
+
+
+
+/// ============================
+///           SETUP
+///          ↓↓↓↓↓↓↓
+int BTN1_PIN = 13;
 int MOTOR1_PIN = 3;
+
+void OnLowTemp(){
+  // Serial.println("Low");
+}
+
+void OnEnterLowTemp(){
+  analogWrite(MOTOR1_PIN, 255);
+}
+
+void OnUpTemp(){
+  // Serial.println("Up");
+}
+
+void OnEnterUpTemp(){
+  analogWrite(MOTOR1_PIN, 0);
+}
+
+Button btn1;
 
 void setup() {
   Serial.begin(9600);
   pinMode(MOTOR1_PIN, 1); // motor 1
   pinMode(BTN1_PIN, INPUT_PULLUP); // button 1
-}
 
-String velocityInput="";
+  btn1.Init(
+    BTN1_PIN,
+    OnLowTemp,
+    OnEnterLowTemp,
+    OnUpTemp,
+    OnEnterUpTemp
+  );
+
+  // btn1.Init(
+  //   BTN1_PIN,
+  //   [](){ Serial.println("Low"); },
+  //   [](){ analogWrite(MOTOR1_PIN, 255); },
+  //   [](){ Serial.println("Up"); },
+  //   [](){ analogWrite(MOTOR1_PIN, 0); }
+  // );
+
+}
+///           ↑↑↑↑↑↑↑
+///            SETUP
+/// ============================
+
+/// ============================
+///          MAIN LOOP
+///          ↓↓↓↓↓↓↓↓
+
+
 void loop() {
   
-  if (IsBtnPressed(BTN1_PIN)){
-    analogWrite(MOTOR1_PIN, 255);
-  } else {
-    analogWrite(MOTOR1_PIN, 0);
-  }
+  // if (digitalRead(BTN1_PIN) == LOW){
+  //   analogWrite(MOTOR1_PIN, 255);
+  // } else {
+  //   analogWrite(MOTOR1_PIN, 0);
+  // }
 
-  if (Serial.available()) { // if there was input
-    char c =Serial.read();
-    if (c!=10){
-      velocityInput+=c;
-    } else {
-      analogWrite(3, velocityInput.toInt());
-
-      Serial.println("Velocity: ");
-      Serial.println(velocityInput);
-      velocityInput="";
-    }
-  }
+  btn1.Update();
 }
+///          ↑↑↑↑↑↑↑↑
+///          MAIN LOOP
+/// ============================
 
-bool IsBtnPressed(int btnPin){
-  /// :param: int pin of the button
-  /// :return: true, if pressed. Else: false
-  
-  return digitalRead(btnPin)==LOW;
-}
